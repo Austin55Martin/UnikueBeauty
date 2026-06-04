55400d55231a3eb6eecd53496793925a6fb70ea8
Austin55Martin <austinmartin55@outlook.com>
2026-06-03 19:53:18 -0700

    adjust the carousel next and prev button wrappers to be larger for on hover events

--- src/Components/InstagramCarousel.tsx
+++ src/Components/InstagramCarousel.tsx
@@ -15,15 +15,37 @@ const useStyles = makeStyles()(() => ({
     minWidth: "100px",
     maxWidth: "500px",
     height: "auto",
+    // The wrapper div for the "next" button
+    "& > div:nth-of-type(2)": {
+      width: "33%",
+      right: "0",
+    },
+    // The wrapper div for the "previous" button
+    "& > div:nth-of-type(3)": {
+      width: "33%",
+      left: "0",
+    },
+    // The right ("next") button
+    "& > div:nth-of-type(2) button": {
+      left: "auto !important",
+      right: "0 !important",
+      position: "absolute",
+    },
+    // The left ("previous") button
+    "& > div:nth-of-type(3) button": {
+      right: "auto !important",
+      left: "0 !important",
+      position: "absolute",
+    },
   },
   img: {
     height: "auto",
     width: "100%",
   },
-  skeletion: {
-    opacity: ".5",
-    width: "350px",
-    height: "400px",
+  fullHeightHoverWrapper: {
+    height: "100%",
+    top: "0",
+    width: "30%",
   },
 }));
 
@@ -35,10 +57,9 @@ function InstagramCarousel() {
       <Carousel
         swipe={true}
         animation="slide"
-        duration={500}
         indicators={true}
         className={classes.carousel}
-        fullHeightHover={true}
+        autoPlay={false}
       >
         {Object.keys(images).map((key, i) => (
           <img key={i} src={images[key]} alt={key} className={classes.img} />
