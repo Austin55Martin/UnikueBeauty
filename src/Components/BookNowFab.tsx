69c994144c8d7db5811882acffe9141908a3e4da
Austin55Martin <austinmartin55@outlook.com>
2026-06-03 19:52:48 -0700

    refactor the fab to its own component

---
+++ src/Components/BookNowFab.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Fab, useMediaQuery, useTheme } from "@mui/material";
+import { makeStyles } from "tss-react/mui";
+
+const useStyles = makeStyles()((theme) => ({
+  floatingButtonStyle: {
+    position: "fixed",
+    right: "1.25rem",
+    bottom: "1.25rem",
+    top: "auto",
+    left: "auto",
+    color: "white",
+    backgroundColor: "#b48c64",
+    "&:hover": {
+      backgroundColor: "#c99c6f",
+    },
+    textTransform: "none",
+  },
+}));
+
+function BookNowFab() {
+  const { classes } = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  if (!isMobile) {
+    return null;
+  }
+
+  return (
+    <Fab
+      variant="extended"
+      size="large"
+      className={classes.floatingButtonStyle}
+    >
+      Book Now
+    </Fab>
+  );
+}
+
+export default BookNowFab;
