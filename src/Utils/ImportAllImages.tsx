952618bfb3aad542b6892bee3c0d8d623730c24a
Austin55Martin <austinmartin55@outlook.com>
2026-06-22 22:47:36 -0700

    reverse order so newer images are first

--- src/Utils/ImportAllImages.tsx
+++ src/Utils/ImportAllImages.tsx
@@ -1,11 +1,13 @@
 // This is a helper to handle importing multiple images from a folder
 function importAll(r: __WebpackModuleApi.RequireContext) {
   let images: Record<string, string> = {};
-  r.keys().map((item, _) => (images[item.replace("./", "")] = r(item)));
+  r.keys()
+    .reverse()
+    .map((item, _) => (images[item.replace("./", "")] = r(item)));
   return images;
 }
 
 const images = importAll(
   require.context("../Gallery/Carousel", false, /\.(png|jpe?g|svg)$/),
 );
-export default images;
+export default images; // Reverse the order of the images so that the most recent ones are first
